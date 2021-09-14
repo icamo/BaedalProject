0bbbe5aada70d333944ca3cb40beccc4e73f2952
User <User@DESKTOP-K9JJN2N>
2021-09-14 11:27:55 +0900

    웹소켓 서버 커밋 / 영민

---
+++ BaedalProjectWeb/src/main/java/webSocket/AbstractRepository.java
@@ -0,0 +1,29 @@
+package webSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class AbstractRepository {
+	private SqlSessionFactory sqlSessionFactory;
+	public AbstractRepository() {
+		setSqlSessionFactory();
+	}
+	private void setSqlSessionFactory() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sqlSessionFactory = 
+				new SqlSessionFactoryBuilder().build(inputStream);
+	}
+	public SqlSessionFactory getSqlSessionFactory() {
+		return sqlSessionFactory;
+	}
+}
---
+++ BaedalProjectWeb/src/main/java/webSocket/MenuInfoDTO.java
@@ -0,0 +1,35 @@
+package webSocket;
+
+public class MenuInfoDTO {
+	String menuName;
+	String menuPrice;
+	String menuCount;
+
+
+	public String getMenuPrice() {
+		return menuPrice;
+	}
+
+	public void setMenuPrice(String menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+
+	public String getMenuCount() {
+		return menuCount;
+	}
+
+	public void setMenuCount(String menuCount) {
+		this.menuCount = menuCount;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	
+	
+
+}
